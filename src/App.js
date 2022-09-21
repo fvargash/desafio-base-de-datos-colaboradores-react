749ca9a846cb157ecade1184082c33da6a2ebcab
franco <fvargash@udd.cl>
2022-09-21 12:55:07 -0400

    subiendo-desafio-lista-colaboradores

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BaseColaboradores } from './BaseColaboradores';
 
 function App() {
+  const [nuevoColaborador, setNuevoColaborador] = useState({nombre:"", correo:""})
+
+  const [listaColaboradores,setListaColaboradores] = useState(BaseColaboradores)
+
+  const [buscadorColaboradores,setBuscadorColaboradores] = useState("") 
+
+  const handleChange = (e,prop) =>{
+    setNuevoColaborador({...nuevoColaborador,[prop]: e.target.value })
+    console.log(nuevoColaborador)
+  }
+
+  const handleClick = () => {
+    setListaColaboradores([...listaColaboradores,{...nuevoColaborador,id:Date.now()}])
+  }  
+
+  const handleSearch = (ev) => {
+    setBuscadorColaboradores(ev.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <h1 className='buscador'>Buscador de Colaboradores:</h1>
+        <input className='input-buscador' placeholder='Buscar un colaborador' type="text" onChange={handleSearch}/>
+      </div>
+      <div className='nombre-colaborador'>
+        <label className='label-nombre'>Nombre Colaborador :</label>
+        <input className='input-nombre' placeholder='Ingresa el nombre del colaborador' type="text" onChange={(ev) => {
+          handleChange(ev,"nombre")
+        }}/>
+      </div>
+      <div className='correo-colaborador'>
+        <label className='label-correo'>Correo Colaborador :</label>
+        <input className='input-correo' placeholder='Ingresa el correo del colaborador' type="text" onChange={(ev) => {
+          handleChange(ev,"correo")
+        }}/>
+      </div>
+        <button className='button' type="submit" onClick={handleClick}>Agregar</button> 
+        <h1 className='titulo-listado'>Listado de colaboradores :</h1>
+      <div className='listado-colaboradores'>
+        <ul>
+          {listaColaboradores.filter((colaborador) => {
+            return(
+            colaborador.nombre.toLowerCase().includes(buscadorColaboradores.toLowerCase()))
+          }).map(colaborador => {
+            return (
+              <li key={colaborador.id}>
+                {colaborador.nombre} - {colaborador.correo}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/BaseColaboradores.js
@@ -0,0 +1,17 @@
+export const BaseColaboradores = [
+    {
+        id:"1",
+        nombre:"colaborador 1",
+        correo:"colaborador1@colaborador1.com"
+    },
+    {
+        id:"1",
+        nombre:"colaborador 2",
+        correo:"colaborador2@colborador2.com"
+    },
+    {
+        id:"3",
+        nombre:"colaborador 3",
+        correo:"colaborador3@colaborador3.com"
+    }
+]
